a26bf1ce46e5260e07065b8a257dbb104e57cd7a
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2023-12-01 17:26:40 -0600

    Crear clase calculadora y sus metodos, sumar, restar, multiplicar y dividir

---
+++ Calculator/Calculator/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class Calculadora
+    {
+        private decimal _a;
+        private decimal _b;
+
+        public decimal Resultado;
+        public Calculadora(decimal a, decimal b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public decimal Sumar() => Resultado = _a + _b;
+        public decimal Restar() => Resultado = _a - _b;
+        public decimal Dividir() => Resultado = _a / _b;
+        public decimal Multiplicar() => Resultado = _a * _b;
+    }
+}
