37bc6cc9791670117f6e8ab3d8db07c03627fb64
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2023-12-01 17:40:30 -0600

    agregado en TestUnit metodos resta, multiplica y divide.

--- Calculator/TestProjectCalculator/UnitTestCalculator.cs
+++ Calculator/TestProjectCalculator/UnitTestCalculator.cs
@@ -18,5 +18,47 @@ namespace TestProjectCalculator
             // assert
             Assert.Equal(expectedResult, actualResult);
         }
+        [Fact]
+        public void TestResta5and3()
+        {
+            // arrange
+            decimal a = 5;
+            decimal b = 3;
+            decimal expectedResult = 2;
+            Calculadora calculadora = new Calculadora(a, b);
+            // act
+            decimal actualResult = calculadora.Restar();
+
+            // assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void TestDivide10and2()
+        {
+            // arrange
+            decimal a = 10;
+            decimal b = 2;
+            decimal expectedResult = 5;
+            Calculadora calculadora = new Calculadora(a, b);
+            // act
+            decimal actualResult = calculadora.Dividir();
+
+            // assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+        [Fact]
+        public void TestMultiplica10and2()
+        {
+            // arrange
+            decimal a = 10;
+            decimal b = 2;
+            decimal expectedResult = 20;
+            Calculadora calculadora = new Calculadora(a, b);
+            // act
+            decimal actualResult = calculadora.Multiplicar();
+
+            // assert
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
