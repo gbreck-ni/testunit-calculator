b01d2158cc80a793d25a5970b45d1a0f874016b2
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2023-12-01 17:47:06 -0600

    agregado, implementacion en program.cs

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+
 Console.WriteLine("Calculadora con Test Unit!");
+
+Console.Write("Numero a: ");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Numero b: ");
+decimal b = Convert.ToDecimal(Console.ReadLine());
+
+Calculadora calc = new Calculadora(a, b);
+
+Console.WriteLine("Suma: {0}", calc.Sumar());
+Console.WriteLine("Resta: {0}", calc.Restar());
+Console.WriteLine("Multiplicación: {0}", calc.Multiplicar());
+Console.WriteLine("División: {0}", calc.Dividir());
 
 Console.ReadKey();
